b9ca94b3797de23ab0de1dd62cf826db0093bfb4
Комер <42230850+MrComer1337@users.noreply.github.com>
2022-09-27 12:18:10 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "//gta5game.vovashmel.host/precache-manifest.js"
+  "//gta5game.vova.website/precache-manifest.js"
 );
 
 self.addEventListener('message', (event) => {
