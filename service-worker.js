54a1f87a75048feb8d1117fcd6994c9edc7595b8
Комер <mrcomer.space@gmail.com>
2023-12-17 09:49:31 +0700

    Updated urls

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "//gta5game.mrcomer.space/precache-manifest.js"
+  "//mrcomer1337.github.io/GTA5GAME/precache-manifest.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -15,7 +15,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//gta5game.mrcomer.space/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//mrcomer1337.github.io/GTA5GAME/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
